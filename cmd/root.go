f40b6cf007582d24087487bb6ab96dd4cec0bdac
James Muranga <jmured@gmail.com>
2019-10-06 21:39:29 -0700

    First pass at the implementation

--- cmd/root.go
+++ cmd/root.go
@@ -1,11 +1,15 @@
 package cmd
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"os"
+	"strings"
 
+	"github.com/inclus/pg-init/pkg/database"
+	_ "github.com/lib/pq"
 	"github.com/spf13/cobra"
-
 	"github.com/spf13/viper"
 )
 
@@ -13,9 +17,24 @@ var cfgFile string
 
 var rootCmd = &cobra.Command{
 	Use:   "pg-init",
-	Short: "short description",
-	Long:  `long description`,
-	Run:   func(cmd *cobra.Command, args []string) {},
+	Short: "wait for database to be ready",
+	Long:  `wait for database to be ready`,
+	Run: func(cmd *cobra.Command, args []string) {
+		db, err := sql.Open("postgres", database.BuildConnectionString())
+		if err != nil {
+			log.Fatalf(fmt.Errorf("failed to create connection %w", err).Error())
+		}
+
+		err = database.WaitForWorkingConnection(db)
+		if err != nil {
+			log.Fatalf(fmt.Errorf("failed to check connection %w", err).Error())
+		}
+
+		err = database.SetupPostgisExtension(db)
+		if err != nil {
+			log.Fatalf(fmt.Errorf("failed to setup postgis extension %w", err).Error())
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -33,5 +52,15 @@ func init() {
 }
 
 func initConfig() {
+	viper.SetDefault("db.host", "localhost")
+	viper.SetDefault("db.port", "5432")
+	viper.SetDefault("db.database", "postgres")
+	viper.SetDefault("db.user", "postgres")
+	viper.SetDefault("db.password", "")
+	viper.SetDefault("db.extra", "sslmode=disable")
+	viper.SetDefault("retry.attempts", 10)
+
+	replacer := strings.NewReplacer(".", "_")
+	viper.SetEnvKeyReplacer(replacer)
 	viper.AutomaticEnv()
 }
